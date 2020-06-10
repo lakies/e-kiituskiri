10cbf404afb4939eef6bb6e531548e53f797e602
Adrian Kirikal <Adrian.Kirikal@playtech.com>
2020-06-10 14:03:45 +0300

    expire token

--- src/main/java/com/vhk/kirjad/security/JWTAuthorizationFilter.java
+++ src/main/java/com/vhk/kirjad/security/JWTAuthorizationFilter.java
@@ -2,6 +2,7 @@ package com.vhk.kirjad.security;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.TokenExpiredException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -36,18 +37,28 @@ public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
             return;
         }
 
-        String token = Arrays.stream(req.getCookies())
-                .filter(cookie -> "accessJwt".equals(cookie.getName()))
-                .map(Cookie::getValue)
+        Cookie cookie = Arrays.stream(req.getCookies())
+                .filter(c -> "accessJwt".equals(c.getName()))
                 .findFirst()
                 .orElse(null);
 
-        if (token == null || !token.startsWith(TOKEN_PREFIX)) {
+        if (cookie == null || cookie.getValue() == null || !cookie.getValue().startsWith(TOKEN_PREFIX)) {
             chain.doFilter(req, res);
             return;
         }
 
-        UsernamePasswordAuthenticationToken authentication = getAuthentication(req);
+
+        UsernamePasswordAuthenticationToken authentication;
+        try {
+            authentication = getAuthentication(req);
+        } catch (TokenExpiredException e) {
+            cookie.setValue(null);
+            cookie.setMaxAge(0);
+            res.addCookie(cookie);
+            log.info("Expiring cookie for user " + req.getRemoteAddr());
+            chain.doFilter(req, res);
+            return;
+        }
 
         SecurityContextHolder.getContext().setAuthentication(authentication);
         chain.doFilter(req, res);
